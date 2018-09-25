09595de99f1252252ecf83fab0b9cac4e2120d1d
formsanguinetti <form.sanguinetti@gmail.com>
2018-09-25 17:04:39 +0100

    creation

---
+++ app/src/main/java/atec/pt/passingobjects/VisualizarGaja.java
@@ -0,0 +1,13 @@
+package atec.pt.passingobjects;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VisualizarGaja extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizar_gajo);
+    }
+}
